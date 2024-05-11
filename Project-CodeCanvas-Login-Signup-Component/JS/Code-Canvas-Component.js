8ac90c44658e29922258d8f4416279286846968b
Dipen patel <pdipen135@gmail.com>
2024-05-11 14:31:52 +0530

    Update Code-Canvas-Component.js

--- Project-CodeCanvas-Login-Signup-Component/JS/Code-Canvas-Component.js
+++ Project-CodeCanvas-Login-Signup-Component/JS/Code-Canvas-Component.js
@@ -7,36 +7,25 @@ function sideBarToggleHandler() {
     const sidebar = document.getElementById("container");
     const mainContent = document.getElementById("main-content");
     if (isSideBarOpen) {
-        // Close the sidebar by adding a class that translates it to the left
         sidebar.classList.add("sidebar-hidden");
-        // Move content to center
         mainContent.classList.add("content-centered");
     } else {
-        // Open the sidebar by removing the class that hides it
         sidebar.classList.remove("sidebar-hidden");
-        // Move content back to original position
         mainContent.classList.remove("content-centered");
     }
-    // Toggle the state of isSideBarOpen
     isSideBarOpen = !isSideBarOpen;
 }
 
 
-// Get all the anchor tags inside the links container
 const links = document.querySelectorAll("#links .link");
 
-// Add click event listeners to each anchor tag
 links.forEach(function(link) {
     link.addEventListener("click", function(event) {
-        // Prevent the default action of the anchor tag
         event.preventDefault();
-
-        // Remove the highlighted-link class from all anchor tags
         links.forEach(function(link) {
             link.classList.remove("highlighted-link");
         });
 
-        // Add the highlighted-link class to the clicked anchor tag
         link.classList.add("highlighted-link");
     });
 });
